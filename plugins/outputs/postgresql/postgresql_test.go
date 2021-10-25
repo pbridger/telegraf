dfcfb16c3772a6f6102bf152cb98789faa507b12
Heikki Hannikainen <hessu@aiven.io>
2021-10-25 18:15:42 +0300

    pg output: postgresql_test metric.New new return value

--- plugins/outputs/postgresql/postgresql_test.go
+++ plugins/outputs/postgresql/postgresql_test.go
@@ -25,22 +25,22 @@ func TestPostgresqlCreateStatement(t *testing.T) {
 	timestamp := time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC)
 
 	var m telegraf.Metric
-	m, _ = metric.New("m", nil, map[string]interface{}{"f": float64(3.14)}, timestamp)
+	m = metric.New("m", nil, map[string]interface{}{"f": float64(3.14)}, timestamp)
 	assert.Equal(t, `CREATE TABLE IF NOT EXISTS "public"."m"(time timestamptz,fields jsonb)`, p.generateCreateTable(m))
 
-	m, _ = metric.New("m", map[string]string{"k": "v"}, map[string]interface{}{"i": int(3)}, timestamp)
+	m = metric.New("m", map[string]string{"k": "v"}, map[string]interface{}{"i": int(3)}, timestamp)
 	assert.Equal(t, `CREATE TABLE IF NOT EXISTS "public"."m"(time timestamptz,tags jsonb,fields jsonb)`, p.generateCreateTable(m))
 
 	p.TagsAsJsonb = false
 	p.FieldsAsJsonb = false
 
-	m, _ = metric.New("m", nil, map[string]interface{}{"f": float64(3.14)}, timestamp)
+	m = metric.New("m", nil, map[string]interface{}{"f": float64(3.14)}, timestamp)
 	assert.Equal(t, `CREATE TABLE IF NOT EXISTS "public"."m"(time timestamptz,"f" float8)`, p.generateCreateTable(m))
 
-	m, _ = metric.New("m", nil, map[string]interface{}{"i": int(3)}, timestamp)
+	m = metric.New("m", nil, map[string]interface{}{"i": int(3)}, timestamp)
 	assert.Equal(t, `CREATE TABLE IF NOT EXISTS "public"."m"(time timestamptz,"i" int8)`, p.generateCreateTable(m))
 
-	m, _ = metric.New("m", map[string]string{"k": "v"}, map[string]interface{}{"i": int(3)}, timestamp)
+	m = metric.New("m", map[string]string{"k": "v"}, map[string]interface{}{"i": int(3)}, timestamp)
 	assert.Equal(t, `CREATE TABLE IF NOT EXISTS "public"."m"(time timestamptz,"k" text,"i" int8)`, p.generateCreateTable(m))
 
 }
